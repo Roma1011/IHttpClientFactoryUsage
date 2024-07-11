5f0b0a91b8926e04afc80c79e6bf086d97c95f34
R.Kekua <romski48@gmail.com>
2024-07-11 22:08:10 +0400

    Add Background Service and Create Client

---
+++ Hosted/Worker.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IHttpClientFactoryUsage.Hosted;
+
+public class Worker([FromServices]IHttpClientFactory factory):BackgroundService
+{
+    private static readonly TimeSpan IntervalMilliSeconds = new(0, 0, 0, 2, 0);
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            var client=factory.CreateClient();
+            await client.GetAsync(new Uri(""),stoppingToken);
+            await Task.Delay(IntervalMilliSeconds, stoppingToken);
+        }
+    }
+}
