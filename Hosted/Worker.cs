41ed95819a761f1793bdaeb55a225b540a0d43fc
R.Kekua <romski48@gmail.com>
2024-07-23 20:03:28 +0400

    Modify

--- Hosted/Worker.cs
+++ Hosted/Worker.cs
@@ -1,13 +1,15 @@
-﻿using System.ComponentModel;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace IHttpClientFactoryUsage.Hosted;
 
 public class Worker([FromServices]IHttpClientFactory factory):BackgroundService
 {
+    private readonly IServiceProvider _serviceProvider;
     private static readonly TimeSpan IntervalMilliSeconds = new(0, 0, 0, 2, 0);
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
+        _serviceProvider.CreateScope();
+        var service=_serviceProvider.GetRequiredService<IHttpClientFactory>();
         while (!stoppingToken.IsCancellationRequested)
         {
             var client=factory.CreateClient();
--- Program.cs
+++ Program.cs
@@ -1,7 +1,11 @@
 using IHttpClientFactoryUsage.Hosted;
 
 var builder = WebApplication.CreateBuilder(args);
+
 builder.Services.AddHostedService<Worker>();
+
 builder.Services.AddHttpClient();
 var app = builder.Build();
+
+
 app.Run();
